471012133046fcfa4e2d002a47257b2772d4cad1
McKenna Black <mckennablack@Simplys-MacBook-Pro-2.local>
2020-04-20 22:10:01 -0600

    edit congress page

---
+++ congress/index.js
@@ -0,0 +1,52 @@
+import { senators } from '../data/senators.js';
+
+const container = document.querySelector('.container');
+
+const fulterSenators = (prop, value) => {
+    return senators.filter(senatro => senator[prop] === value);
+}
+
+function simplifiedSenators(senatorArray) {
+    return senatorArray.map(senator => {
+        let middleName = senator.middle_name ? ` ${senator.middle_name}` : ` `
+        return {
+            id: senator.id,
+            name: `${senator.first_name}${middlename}${senator.last_name}`,
+            imgURL: `https://www.govtrack.us/static/legislator-photos/${senator.govtrack_id}-200px.jpeg`,
+            seniority: parseInt(senator.seniority, 10),
+            twitterAccount: senator.twitter_account;
+        }
+    })
+}
+
+function populateContainer(smallSenatorsArray) {
+    console.log(smallSenatorsArray)
+    smallSenatorsArray.forEach(senator => {
+
+        let senFigure = document.createElement('figure');
+        let figImg = document.createElement('img');
+        let figCaption = document.createElement('figcaption')
+
+        fitImg.src = senator.imgURL;
+        figCaption.textContent = senator.name;
+
+        senFigure.appendChild(figImg);
+        senFigure.appendChild(figCaption);
+        container.appendChild(senFigure);
+    })
+}
+
+const republicans = filterSenators('party', 'R');
+const democrats = filterSenators('party', 'D');
+
+const mostSeniority = simplefiedSenators(republicans).reduce(
+    (acc, senator) => {
+    return acc.seniority > senator.seniority ? acc : senator;
+}
+)
+
+const twitter = simplifiedSenators(democrates).reduce((acc, senator) => acc.twitterAccount)
+
+console.log(mostSeniority)
+
+populateContainer(mappedSenators);
